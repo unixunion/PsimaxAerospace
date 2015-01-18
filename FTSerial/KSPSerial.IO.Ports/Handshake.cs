19623a6a3c1efb5053ec62b378667fe3cc0a88c6
Kegan Holtzhausen <marzubus@gmail.com>
2015-01-18 21:32:39 +0100

    fixes

---
+++ FTSerial/KSPSerial.IO.Ports/Handshake.cs
@@ -0,0 +1,22 @@
+//
+// System.IO.Ports.Handshake.cs
+//
+// Authors:
+//	Chris Toshok (toshok@ximian.com)
+//
+//namespace KSPSerial.IO.Ports Inc. (http://www.novell.com)
+//
+
+namespace KSPSerial.IO.Ports
+
+{
+	public enum Handshake 
+	{
+		None,
+		XOnXOff,
+		RequestToSend,
+		RequestToSendXOnXOff
+	} 
+}
+
+
---
+++ FTSerial/KSPSerial.IO.Ports/StopBits.cs
@@ -0,0 +1,20 @@
+//
+// System.IO.Ports.StopBits.cs
+//
+// Authors:
+//	Chris Toshok (toshok@ximian.com)
+//
+// (c) Copyright 2006 Novell, Inc. (http://www.novell.com)
+//
+
+namespace KSPSerial.IO.Ports 
+{
+	public enum StopBits 
+	{
+		None,
+		One,
+		Two,
+		OnePointFive
+	} 
+}
+
